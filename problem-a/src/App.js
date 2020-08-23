ef16caf56b1059554e39610f488976f49a4950dd
sc249 <sc249@uw.edu>
2020-08-22 22:20:08 -0700

    problem-a

--- problem-a/src/App.js
+++ problem-a/src/App.js
@@ -6,3 +6,64 @@ const EXAMPLE_SENATORS = [
 ];
 
 /* Your code goes here */
+
+export class App extends Component {
+  render() {
+    let senators = this.props.senators;
+
+    return (
+      <div className="container">
+        <h1>US Senators 2019</h1>
+        <SenatorTable senators={senators} key={senators}/>
+      </div>
+    );
+  }
+}
+
+export class SenatorTable extends Component {
+  render() {
+    let senatRow = this.props.senators.map((x) => <SenatorRow key={x.id} senator={x}/>);
+    return (
+      <table className = "table table-bordered">
+        <TableHeader cols={['Name', 'State', 'Phone', 'Twitter']} />
+        <tbody>
+          {senatRow}
+        </tbody>
+      </table>
+    )
+  }
+}
+
+export class TableHeader extends Component {
+  render() {
+    let cols = this.props.cols.map(x => 
+      <th key={x}>{x}</th>);
+      
+    return (
+      <thead>
+        <tr>
+          {cols}
+        </tr>
+      </thead>
+    );
+    
+  }
+  
+}
+
+export class SenatorRow extends Component {
+  render() {
+    let senator = this.props.senator;
+  
+
+    return (
+      <tr>
+        <td>{senator.name}</td>
+        <td>{senator.party.charAt(0) + ' - ' + senator.state}</td>
+        <td><a href={"tel:" + senator.phone}>{senator.phone}</a></td>
+        <td><a href={'https://twitter.com/' + senator.twitter}>{'@' + senator.twitter}</a> </td>
+      </tr>
+    );
+  }
+  
+}
--- problem-a/src/index.js
+++ problem-a/src/index.js
@@ -2,3 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 //render the App component here!
+import {App} from './App';
+import senators from './senators.json';
+
+
+ReactDOM.render(<App senators={senators}/>, document.getElementById('root'));
